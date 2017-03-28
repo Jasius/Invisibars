61d7dbebfdbe9f3b47d75f63be6b012bd7716cdb
Mindaugas Jasiulis <jasius@outlook.com>
2017-03-29 00:35:49 +0300

    func addition

--- src/js/insert.js
+++ src/js/insert.js
@@ -1,2 +1,12 @@
 document.body.classList.add("visibar");
 alert("Extension disabled for this session.");
+
+// function invfunc(div) {
+//   var className = div.getAttribute("class");
+//   if(className=="inactive") {
+//     div.className = "active";
+//   }
+//   else{
+//     div.className = "inactive";
+//   }
+// }
